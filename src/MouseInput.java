cf0c0c5618f4b39d3a93e8c11aad53aa19937d74
Autumn <abertram2021@gmail.com>
2022-11-20 16:51:30 -0500

    Testing in-gmae menu and options state

--- src/MouseInput.java
+++ src/MouseInput.java
@@ -18,22 +18,28 @@ public class MouseInput implements MouseListener {
         //    public Rectangle startButton = new Rectangle(MazeGame.WIDTH / 2 + 110, 150, 120, 50);
         //    public Rectangle optionsButton = new Rectangle(MazeGame.WIDTH / 2 + 110, 250, 120, 50);
         //    public Rectangle exitButton = new Rectangle(MazeGame.WIDTH / 2 + 110, 350, 120, 50);
-
-        //start button
-        if (mx >= MazeGame.WIDTH / 2 +110 && mx <= MazeGame.WIDTH / 2 + 220){
-            if (my >= 150 && my <= 200){
-                //pressed start button
-                MazeGame.state = MazeGame.STATE.GAME;
+        if (MazeGame.getState() == MazeGame.STATE.MENU){
+            //start button
+            if (mx >= MazeGame.WIDTH / 2 +110 && mx <= MazeGame.WIDTH / 2 + 220){
+                if (my >= 150 && my <= 200){
+                    //pressed start button
+                    MazeGame.state = MazeGame.STATE.GAME;
+                }
             }
+
+            //options button
+
+
+            //exit button
+            if (mx >= MazeGame.WIDTH / 2 +110 && mx <= MazeGame.WIDTH / 2 + 220){
+                if (my >= 350 && my <= 400){
+                    //pressed exit button
+                    System.exit(1);
+                }
+            }
+
         }
 
-        //exit button
-        if (mx >= MazeGame.WIDTH / 2 +110 && mx <= MazeGame.WIDTH / 2 + 220){
-            if (my >= 350 && my <= 400){
-                //pressed exit button
-                System.exit(1);
-            }
-        }
 
     }
 
