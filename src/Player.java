66d9377f3b534a7d2374d0a79445fa7d5ee2d2dc
xxaleebxx <112494911+xxaleebxx@users.noreply.github.com>
2022-12-08 10:42:17 -0500

    update player size

--- src/Player.java
+++ src/Player.java
@@ -20,7 +20,7 @@ public class Player {
 
         SpriteSheet ss = new SpriteSheet(game.getSpriteSheet());
 
-        player = ss.grabImage(1, 1, 16, 16);
+        player = ss.grabImage(1, 1, 15, 15);
         //change coordinates depending on future sprite sheet
     }
 
